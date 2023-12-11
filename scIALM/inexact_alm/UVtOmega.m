80dc388066912bc905f54d192f114f42bf3e754c
lxhfighting <90842103+lxhfighting@users.noreply.github.com>
2023-12-11 13:00:27 +0800

    Delete scIALM/inexact_alm directory

--- scIALM/inexact_alm/UVtOmega.m
+++
@@ -1,9 +0,0 @@
-function y = UVtOmega(M,I,J,col);
-
-y = zeros(length(I), 1);
-for k = 1:length(col)-1
-    j = J(col(k)+1);
-    Xj = M(:,j);
-    idx = [col(k)+1:col(k+1)];
-    y(idx) = Xj(I(idx));
-end
